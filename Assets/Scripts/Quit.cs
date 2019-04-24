fc14ca5b446cdfd27b79c33293dd7bbec0a39bf1
Caleb Segenet <csegenet0@gmail.com>
2019-04-24 16:45:21 -0700

    Beginning main menu implementation, initial asset aggregation

---
+++ Assets/Scripts/Quit.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Quit : MonoBehaviour {
+
+    public void doExitGame()
+    {
+        Debug.Log("we quit boyz");
+        Application.Quit();
+    }
+}
