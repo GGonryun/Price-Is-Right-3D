bb07a9621c365f687f4c116e51a1f7aeb824c252
David Itsygin <daviditsygin@gmail.com>
2019-04-26 16:27:18 -0700

    impact receiver - apply force on spell collision

---
+++ Assets/ImpactReceiver.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Collections;
+public class ImpactReceiver : MonoBehaviour {
+
+	// Use this for initialization
+	public float forceApplied = 1f;
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnParticleCollision (GameObject col){
+		Debug.Log("COLLISION");
+		Debug.Log(col.tag);
+		if (col.tag == "Spells"){
+			Debug.Log("SPELL COLLISION");
+			Vector3 moveDirection = transform.position - col.transform.position;
+			gameObject.GetComponent<Rigidbody>().AddForce (moveDirection * forceApplied);
+		}
+	}
+}
