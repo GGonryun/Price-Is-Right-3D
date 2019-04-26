5c078bc0a75e57e8ee3d84001e16aac5912af92d
Miguel Campos <amodestduck@gmail.com>
2019-04-25 23:49:06 -0700

    Add Login
    Feature
    Add new Login UI and Lobby System.
    -

--- Assets/Miguel/Lobby/LobbyManager.cs
+++ Assets/Miguel/Lobby/LobbyManager.cs
@@ -1,18 +1,59 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+using Photon.Pun;
+using Photon.Realtime;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class LobbyManager : MonoBehaviour
+public class LobbyManager : MonoBehaviourPunCallbacks
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("Login Panel")]
+    [Tooltip("")]
+    [SerializeField]
+    private GameObject loginPanel;
+
+    [Tooltip("")]
+    [SerializeField]
+    private InputField playerNameInputField;
+
+    [Tooltip("")]
+    [SerializeField]
+    private Button loginButton;
+
+    #region UNITY CALLBACKS
+    private void Awake()
     {
-        
+        PhotonNetwork.AutomaticallySyncScene = true;
+
+        playerNameInputField.text = $"Player #{Random.Range(0, System.Int32.MaxValue)}";
+    }
+    #endregion UNITY CALLBACKS
+
+    #region PUN CALLBACKS
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("PUN has connected to master server ! ");
     }
 
-    // Update is called once per frame
-    void Update()
+    public override void OnDisconnected(DisconnectCause cause)
     {
-        
+        Debug.LogWarning($"PUN has disconnected w/ reason {cause}");
     }
+    #endregion PUN CALLBACKS
+
+    #region UI CALLBACKS
+    public void OnClickLogin()
+    {
+        string playerName = playerNameInputField.text;
+
+        if (playerName.Equals(""))
+            return;
+
+        PhotonNetwork.LocalPlayer.NickName = playerName;
+        PhotonNetwork.GameVersion = _version;
+        PhotonNetwork.ConnectUsingSettings();
+    }
+    #endregion UI CALLBACKS
+
+    #region PRIVATES
+    private const string _version = "1";
+    #endregion PRIVATES
 }
