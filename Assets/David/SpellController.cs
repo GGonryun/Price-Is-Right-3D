4ccf33130d367ecc4bf5cc3b1eebb4154bafed23
Miguel Campos <amodestduck@gmail.com>
2019-04-27 16:22:40 -0700

    Improving file structure

---
+++ Assets/David/SpellController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpellController : MonoBehaviour {
+	public float damage = 10f;
+	public float force = 50f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
